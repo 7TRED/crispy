b9b9d47a9590eca0cb9dd8c2a1d6d06819c8d73d
7TRED <ms1514094@gmail.com>
2023-02-18 21:56:51 +0530

    created a function to generate start_urls

--- services/scraper_service/news_scraper/news_scraper/spiders/BGRIndia.py
+++ services/scraper_service/news_scraper/news_scraper/spiders/BGRIndia.py
@@ -1,10 +1,14 @@
 import scrapy
 from news_scraper.items import NewsScraperItem
 
+def getStartUrls(num_pages):
+    return [f"https://www.techlusive.in/news/page/{i}" for i in range(1,num_pages+1)]
+
 class BgrindiaSpider(scrapy.Spider):
     name = "BGRIndia"
+    num_pages=5
     allowed_domains = ["www.techlusive.in"]
-    start_urls = ["https://www.techlusive.in/news/page/1","https://www.techlusive.in/news/page/2","https://www.techlusive.in/news/page/3","https://www.techlusive.in/news/page/4","https://www.techlusive.in/news/page/5"]
+    start_urls = getStartUrls(num_pages)
 
     def parse(self, response):
         articleLinks=response.css("div.media>div.media-left>a::attr(href)").getall()
