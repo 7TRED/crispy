367bc7a32e3079f9ce7551587480a71f4c3c5245
7TRED <ms1514094@gmail.com>
2023-02-19 01:59:24 +0530

    refactored code of spiders

--- services/scraper_service/news_scraper/news_scraper/spiders/BGRIndia.py
+++ services/scraper_service/news_scraper/news_scraper/spiders/BGRIndia.py
@@ -11,16 +11,16 @@ class BgrindiaSpider(scrapy.Spider):
     start_urls = getStartUrls(num_pages)
 
     def parse(self, response):
-        articleLinks=response.css("div.media>div.media-left>a::attr(href)").getall()
+        articleLinks=response.css("div.media > div.media-left > a::attr(href)").getall()
         return response.follow_all(articleLinks,self.parseArticle)
     
     def parseArticle(self,response):
-        content=response.css("div.article-content>p::text").getall()
+        content=response.css("div.article-content > p::text").getall()
         content=" ".join(content)
 
         item=NewsScraperItem();
         item['title']=response.css("h1::text").get();
         item['content']=content;
-        item['date']=response.css("li.publish>meta::attr(content)").get();
+        item['date']=response.css("li.publish > meta::attr(content)").get();
         item['url']=response.url;
         yield item;
