ecd21ac2216a71b78cd4cc5f9f093f6ff8b36a07
nishu rai <rainishu111@gmail.com>
2023-02-18 21:25:28 +0530

    modified scraper for BGR

--- services/scraper_service/news_scraper/news_scraper/spiders/BGRIndia.py
+++ services/scraper_service/news_scraper/news_scraper/spiders/BGRIndia.py
@@ -4,18 +4,18 @@ from news_scraper.items import NewsScraperItem
 class BgrindiaSpider(scrapy.Spider):
     name = "BGRIndia"
     allowed_domains = ["www.techlusive.in"]
-    start_urls = ["https://www.techlusive.in/"]
+    start_urls = ["https://www.techlusive.in/news/page/1","https://www.techlusive.in/news/page/2","https://www.techlusive.in/news/page/3","https://www.techlusive.in/news/page/4","https://www.techlusive.in/news/page/5"]
 
     def parse(self, response):
-        articleLinks=response.css("a.mob_text_ln_aLink::attr(href)").getall()
+        articleLinks=response.css("div.media>div.media-left>a::attr(href)").getall()
         return response.follow_all(articleLinks,self.parseArticle)
     
     def parseArticle(self,response):
-        content=response.css("div.article-content > p::text").getall()
+        content=response.css("div.article-content>p::text").getall()
         content=" ".join(content)
 
         item=NewsScraperItem();
-        item['title']=response.css("h1.title_name::text").get();
+        item['title']=response.css("h1::text").get();
         item['content']=content;
         item['date']=response.css("li.publish>meta::attr(content)").get();
         item['url']=response.url;
