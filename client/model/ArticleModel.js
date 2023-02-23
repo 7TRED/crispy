93c67e5d57b9eebef64c5583686f3b3a3370d258
nishu rai <rainishu111@gmail.com>
2023-02-24 01:31:25 +0530

    news API created

--- client/model/ArticleModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const ArticleSchema = new mongoose.Schema({
-    name: String,
-    email: String
-});
-module.exports = mongoose.model('Article', ArticleSchema);
---
+++ services/summary_service/src/index.ts
@@ -0,0 +1,40 @@
+const express = require('express');
+import dotenv from 'dotenv';
+import { Application, Request, Response } from 'express';
+import mongoose, { ConnectOptions } from 'mongoose';
+const { Article } = require('./model/ArticleModel');
+
+dotenv.config();
+const app: Application = express();
+const port: number = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.MONGO_URI!, { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions);
+
+app.get('/', async (_req: Request, res: Response) => {
+    try {
+        const articles = await Article.find({});
+        res.send(articles);
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+app.post('/article', async (req: Request, res: Response) => {
+    try {
+        const article = new Article({
+            name: req.body.name,
+            email: req.body.email
+        });
+        await article.save();
+        res.send('Article saved!!');
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening at port ${port}`);
+});
